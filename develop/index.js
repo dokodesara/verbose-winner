504b554b688257c131e73f3351a66b02d191803b
dokodesara <dokodesara@gmail.com>
2022-02-25 23:36:35 -0500

    functionwrite readme file

--- develop/index.js
+++ develop/index.js
@@ -1,20 +1,37 @@
 //  Packages requirer for this application
-const inquirer = require ('inquirer')
+const inquirer = require ('inquirer');
 const fs = require('fs');
-const generateREADME = ({title, description, license, story, git, email})
+const util =require('util');
 
-// TODO: Create an array of questions for user input
+const generatorMarkdown = require('./util/generateMarkdown');
+
+// Creating an array of questions for user input
 const questions = [];
 inquirer.prompt([
     {
       type: 'input',
-      name: 'title',
+      name: 'Title',
       message: 'What is your project title?',
     },
     {
         type: 'input',
-        name: 'description',
+        name: 'Description',
         message: 'Please write a short description of your project.',
+      },
+      {
+        type: 'input',
+        name: 'Table of Contents',
+        message: 'Table of Contents',
+      },
+      {
+        type: 'input',
+        name: 'Installation',
+        message: 'What does the user need to install to use this app?',
+      },
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'How is the app used ? Give instructions',
       },
     {
       type: 'checkbox',
@@ -25,10 +42,16 @@ inquirer.prompt([
         'GNU General Public License v3.0',
         'MIT License'],
     },
+    
     {
         type: 'input',
-        name: 'story',
-        message: 'What is your user story?',
+        name: 'Contributing',
+        message: 'Who contributet to this project?',
+      },
+      {
+        type: 'input',
+        name: 'Testing',
+        message: 'What command are nedded to test this app?.',
       },
       {
         type: 'input',
@@ -41,24 +64,32 @@ inquirer.prompt([
         message: 'What is your email adress?',
       },   
   ])
-  .then((answers) => {
-      const README = generateREADME(answers)
+
     
 
 
-// TODO: Create a function to write README file
-fs.writeToFile('README.md', README,(err) =>
-      err ? console.error(err) : console.log('Successfully generated a README file!'));
-    
-   
-      
-  });
+// Creating a function to write README file
+function writeToFile(fileName, data) {
+   console.log(fileName);
+   console.log(data);
+   if (err) {
+       return console.log (err)
+   }else console.log (success)
+}   
 
 
-// TODO: Create a function to initialize app
+
+
+// Creating a function to initialize app
 function init() {
-
+    inquirer.prompt(questions)
+    .then (function(data) {
+        writeToFile ('README.md',generatorMarkdown(data));
+        console.log(data)
+    })
 }
+
+
 
 // Function call to initialize app
 init();
